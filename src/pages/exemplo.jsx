947a4ec1f3e658f2b9094c96ccd3521f6fdd3bf7
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-04-12 11:35:23 -0300

    Navegação

--- src/pages/exemplo.jsx
+++ src/pages/exemplo.jsx
@@ -1,8 +1,10 @@
 import Cabecalho from "../components/Cabecalho";
+import Link from "next/link";
 
 export default function Exemplo() {
   return (
     <>
+      <Link href="/">Voltar</Link>
       <Cabecalho titulo="Next.js & React" />
       <Cabecalho titulo="Aprenda Next.js na pratica" />
     </>
