1df474cfd6797dcdeaa1c617fd455fee421aaeaa
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-04-12 11:19:13 -0300

    Module css

--- src/pages/estiloso.jsx
+++ src/pages/estiloso.jsx
@@ -1,9 +1,9 @@
-import styles from '../styles/estiloso.module.css'
+import styles from "../styles/estiloso.module.css";
 
 export default function Estiloso() {
-    return (
-        <div className={styles.roxo}>
-            <h1>Estilo usando Css Módulos</h1>
-        </div>
-    )
+  return (
+    <div className={styles.roxo}>
+      <h1>Estilo usando Css Módulos</h1>
+    </div>
+  );
 }
