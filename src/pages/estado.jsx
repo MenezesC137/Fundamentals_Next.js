82a5b6c5d6f0a56ddf8918d55b3158dc31409d4f
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-04-13 08:44:26 -0300

    Comp. com estado

---
+++ src/pages/estado.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import Layout from '../components/Layout'
+
+export default function Estado() {
+   
+    const [state, setState] = useState(0)
+
+    function incrementar(){
+        setState(state + 1)
+    }
+
+    return (
+        <Layout titulo="Componente com Estado">
+            <div>
+                {state}
+            </div>
+            <button onClick={incrementar}>
+                Incrementar
+            </button>
+        </Layout>
+    )
+}
